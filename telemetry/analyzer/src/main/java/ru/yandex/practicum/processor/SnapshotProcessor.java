ca5807f8cfe6121abf7ac67a04ceb828229ebe5b
Daniil Andreev <daniiland9@yandex.ru>
2025-03-29 11:00:24 +0300

    fix: analyzer

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/processor/SnapshotProcessor.java
@@ -0,0 +1,60 @@
+package ru.yandex.practicum.processor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.errors.WakeupException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.handler.snapshot.SensorsSnapshotHandler;
+import ru.yandex.practicum.kafka.KafkaClient;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+
+import java.time.Duration;
+import java.util.List;
+
+@Slf4j
+@Component
+public class SnapshotProcessor {
+    private final SensorsSnapshotHandler sensorsSnapshotHandler;
+    private final Consumer<String, SensorsSnapshotAvro> snapshotConsumer;
+
+    @Value("${collector.kafka.topics.snapshots-events}")
+    private String snapshotEventsTopic;
+
+    private static final Duration CONSUME_ATTEMPT_TIMEOUT = Duration.ofMillis(1000);
+
+    public SnapshotProcessor(SensorsSnapshotHandler sensorsSnapshotHandler, KafkaClient kafkaClient) {
+        this.sensorsSnapshotHandler = sensorsSnapshotHandler;
+        this.snapshotConsumer = kafkaClient.getKafkaSnapshotConsumer();
+    }
+
+    public void start() {
+        Runtime.getRuntime().addShutdownHook(new Thread(snapshotConsumer::wakeup));
+        try {
+            snapshotConsumer.subscribe(List.of(snapshotEventsTopic));
+            while (true) {
+                ConsumerRecords<String, SensorsSnapshotAvro> records = snapshotConsumer.poll(CONSUME_ATTEMPT_TIMEOUT);
+                for (ConsumerRecord<String, SensorsSnapshotAvro> record : records) {
+
+                    log.info("{}: Полученное сообщение из kafka: {}", SnapshotProcessor.class.getSimpleName(), record);
+                    sensorsSnapshotHandler.handle(record.value());
+                }
+                snapshotConsumer.commitAsync();
+            }
+        } catch (WakeupException ignored) {
+
+        } catch (Exception e) {
+            log.error("{}: Ошибка во время обработки snapshot", SnapshotProcessor.class.getSimpleName(), e);
+        } finally {
+            try {
+                snapshotConsumer.commitSync();
+            } finally {
+                log.info("{}: Закрываем консьюмер", SnapshotProcessor.class.getSimpleName());
+                snapshotConsumer.close();
+            }
+        }
+    }
+}
+
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/handler/hub/HubEventHandler.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.telemetry.analyzer.handler.hub;
-
-import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
-
-public interface HubEventHandler {
-    String getHubEventType();
-
-    void handle(HubEventAvro event);
-}
