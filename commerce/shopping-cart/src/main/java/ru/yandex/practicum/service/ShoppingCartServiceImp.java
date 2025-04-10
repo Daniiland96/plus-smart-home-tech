caf84f860f528157d2a0a65e41b370918e2262c4
Daniil Andreev <daniiland9@yandex.ru>
2025-04-10 23:08:51 +0300

    add: ShoppingCart

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/service/ShoppingCartServiceImp.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/service/ShoppingCartServiceImp.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.dto.ShoppingCartDto;
+import ru.yandex.practicum.feignClient.ShoppingStoreFeignClient;
 import ru.yandex.practicum.repository.ShoppingCartRepository;
 
 @Slf4j
@@ -11,9 +12,18 @@ import ru.yandex.practicum.repository.ShoppingCartRepository;
 @RequiredArgsConstructor
 public class ShoppingCartServiceImp implements ShoppingCartService {
     private final ShoppingCartRepository cartRepository;
+    private final ShoppingStoreFeignClient shoppingStoreFeignClient;
 
     @Override
     public ShoppingCartDto addProductInShoppingCart(String userName, ShoppingCartDto shoppingCartDto) {
+        if (userName == null || userName.isBlank()){
+            throw new IllegalArgumentException("Имя пользователя не может быть null или пустым");
+        }
+        if (shoppingCartDto == null) {
+            throw new IllegalArgumentException("ShoppingCartDto не может быть null");
+        }
+
         return null;
     }
+
 }
