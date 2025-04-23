79a437811e21485f0cdad0412d35007973f1e053
Daniil Andreev <daniiland9@yandex.ru>
2025-04-23 07:35:05 +0300

    add: Payment

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/api/payment/PaymentApi.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.api.payment;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.yandex.practicum.dto.order.OrderDto;
+import ru.yandex.practicum.dto.payment.PaymentDto;
+
+public interface PaymentApi {
+    @PostMapping("/api/v1/payment")
+    PaymentDto addProductInShoppingCart(@Valid @RequestBody OrderDto orderDto);
+}
